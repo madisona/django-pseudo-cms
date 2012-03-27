3e3d89db15dac544ef99467b18a546530b9a5d54
Aaron Madison <aaron.l.madison@gmail.com>
2012-03-27 06:19:33 -0500

    added methods to convert plain text to reST or linebreaks to <br />

---
+++ pseudo_cms/tests/test_utils.py
@@ -0,0 +1,31 @@
+
+
+from django.test import TestCase
+
+from pseudo_cms import utils
+
+
+class ConvertToHTMLTests(TestCase):
+
+    reST_text = "This is text\n"\
+                "==============="
+    plain_text = "This is text\nThat's Cool"
+    html = "<h4>This is the title</h4>"
+
+    def test_turns_rst_to_html(self):
+        self.assertTrue("<h4>This is text</h4>" in utils.reST_to_html(self.reST_text))
+
+    def test_turns_text_to_html_line_breaks(self):
+        self.assertEqual("This is text<br />That's Cool", utils.text_to_html(self.plain_text))
+
+    def test_convert_to_html_uses_rst(self):
+        result = utils.convert_to_html(self.reST_text, utils.reST)
+        self.assertTrue("<h4>This is text</h4>" in result)
+
+    def test_convert_to_html_uses_html(self):
+        result = utils.convert_to_html(self.html, utils.HTML)
+        self.assertTrue("<h4>This is the title</h4>" in result)
+
+    def test_convert_to_html_uses_plain_text(self):
+        result = utils.convert_to_html(self.plain_text, utils.PLAIN_TEXT)
+        self.assertTrue("This is text<br />That's Cool" in result)
---
+++ pseudo_cms/utils.py
@@ -0,0 +1,39 @@
+
+from docutils.core import publish_parts
+
+from django.conf import settings
+from django.utils.encoding import smart_str
+from django.template.defaultfilters import linebreaksbr
+
+HTML = u"html"
+PLAIN_TEXT = u"text"
+reST = u"reST"
+CONTENT_FORMAT_CHOICES = (
+    (HTML, u'Raw HTML'),
+    (reST, u'reStructuredText'),
+    (PLAIN_TEXT, u"Plain Text"),
+)
+
+DOCUTILS_SETTINGS = getattr(settings, 'DOCUTILS_SETTINGS',
+    {  'doctitle_xform': False,
+       'initial_header_level': 4,
+       'id_prefix': 's-',
+    }
+)
+
+def reST_to_html(text):
+    return publish_parts(source=smart_str(text),
+                         writer_name="html",
+                         settings_overrides=DOCUTILS_SETTINGS)['fragment']
+
+def text_to_html(text):
+    return linebreaksbr(text)
+
+def convert_to_html(text, content_type):
+    if content_type == PLAIN_TEXT:
+        html = text_to_html(text)
+    elif content_type == reST:
+        html = reST_to_html(text)
+    else:
+        html = text
+    return html
