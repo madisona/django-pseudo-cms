c421c4e8a67e5bb1e1624489638066b7054f34b7
Aaron Madison <aaron.l.madison@gmail.com>
2018-02-08 06:59:38 -0600

    Peripheral: removed unnecessary test imports

--- pseudo_cms/tests/__init__.py
+++
@@ -1,4 +0,0 @@
-from pseudo_cms.tests.test_models import *
-from pseudo_cms.tests.test_templatetags import *
-from pseudo_cms.tests.test_utils import *
-from pseudo_cms.tests.test_validators import *
