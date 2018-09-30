1645461d7c4106f6ffde035258d40a00758b6b99
Aaron Madison <aaron.l.madison@gmail.com>
2018-09-29 20:04:03 -0500

    Peripheral: made tests be ok for django 1.* and 2.*

--- pseudo_cms/tests/test_utils.py
+++ pseudo_cms/tests/test_utils.py
@@ -10,13 +10,17 @@ class ConvertToHTMLTests(TestCase):
     plain_text = "This is text\nThat's Cool"
     html = "<h4>This is the title</h4>"
 
+    def _strip_tag(self, val):
+        # django 2.0 uses plain <br>. Prior uses <br />
+        return val.replace("<br />", "<br>")
+
     def test_turns_rst_to_html(self):
         self.assertTrue(
             "<h4>This is text</h4>" in utils.reST_to_html(self.reST_text))
 
     def test_turns_text_to_html_line_breaks(self):
-        self.assertEqual("This is text<br />That&#39;s Cool",
-                         utils.text_to_html(self.plain_text))
+        self.assertEqual("This is text<br>That&#39;s Cool",
+                         self._strip_tag(utils.text_to_html(self.plain_text)))
 
     def test_convert_to_html_uses_rst(self):
         result = utils.convert_to_html(self.reST_text, utils.reST)
@@ -28,4 +32,4 @@ class ConvertToHTMLTests(TestCase):
 
     def test_convert_to_html_uses_plain_text(self):
         result = utils.convert_to_html(self.plain_text, utils.PLAIN_TEXT)
-        self.assertTrue("This is text<br />That&#39;s Cool" in result)
+        self.assertTrue("This is text<br>That&#39;s Cool" in self._strip_tag(result))
